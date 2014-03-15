c93f9fcdaee035a1a43ee25fe423d2bba7aff04e
Dovydas <dovydas.kukalis@gmail.com>
2014-03-15 15:59:11 +0200

    fix

--- library.js
+++ library.js
@@ -4,11 +4,6 @@
 	var Twitter = {},
 		embed = '<span data-url="https://twitter.com/$2/statuses/$3"></span><script type="text/javascript" src="https://api.twitter.com/1/statuses/oembed.json?id=$3&callback=twitterEmbed"></script>';
 
-	Twitter.addScripts = function(scripts, callback) {
-                return scripts.concat([
-                                'plugins/twitter/main.js'
-                        ]);
-        };
 	Twitter.parse = function(postContent, callback) {
 		var	regularUrl = /<a href="(http|https):\/\/twitter.com\/([^\/"\s]*)\/status\/([^\/"\s]*)">.+?<\/a>/g
 
