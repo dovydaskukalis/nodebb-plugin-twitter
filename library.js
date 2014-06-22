368889a6e81fe890e69ac8c55564f1adf5d85577
a5mith <aaronsmith91@sky.com>
2014-06-22 08:18:32 +0100

    or not

--- library.js
+++ library.js
@@ -5,7 +5,7 @@
 		embed = '<span data-url="https://twitter.com/$2/statuses/$3"></span><script type="text/javascript" src="//api.twitter.com/1/statuses/oembed.json?id=$3&callback=twitterEmbed"></script>';
 
 	Twitter.parse = function(postContent, callback) {
-		var	regularUrl = /<a href="(http|https):\/\/twitter.com\/([^\/"\s]*)\/(status|statuses)\/([^\/"\s]*)(\/photo\/1|)">.+?<\/a>/g
+		var	regularUrl = /<a href="(http|https):\/\/twitter.com\/([^\/"\s]*)\/status\/([^\/"\s]*)(\/photo\/1|)">.+?<\/a>/g
 
 		 if (postContent.match(regularUrl)) {
 			postContent = postContent.replace(regularUrl, embed);
