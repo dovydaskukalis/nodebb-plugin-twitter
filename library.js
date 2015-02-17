044b6f54513ab289489472786687d54aa9ad497f
dovydaskukalis <dovydas.kukalis@gmail.com>
2015-02-17 21:49:10 +0000

    Fixes

--- library.js
+++ library.js
@@ -1,27 +1,27 @@
- (function(module) {
-	"use strict";
+(function(module) {
+    "use strict";
 
-	var Twitter = {},
-		embed = '<span data-url="https://twitter.com/$2/statuses/$3"></span><script type="text/javascript" src="//api.twitter.com/1/statuses/oembed.json?id=$3&callback=twitterEmbed"></script>';
+    var Twitter = {},
+        embed = '<blockquote class="content twitter-tweet" lang="en"><a href="https://twitter.com/$1/status/$2"></a></blockquote><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>';
 
-	Twitter.parse = function(data, callback) {
-		var	regularUrl = /<a href="(http|https):\/\/twitter.com\/([^\/"\s]*)\/status\/([^\/"\s]*)(\/photo\/1|)">.+?<\/a>/g
-		var postContent = data && data.postData && data.postData.content;
+    Twitter.parse = function(data, callback) {
+        var regularUrl = /<a href="(?:https?:\/\/)?(?:twitter\.com)\/([^\/"\s]*)\/statuse?s?\/([^\/"\s]*)(\/photo\/1|)">.+?<\/a>/g
+        var postContent = data && data.postData && data.postData.content;
 
-		if (postContent && postContent.match(regularUrl)) {
-			data.postData.content = postContent.replace(regularUrl, embed);
-		}
+        if (postContent && postContent.match(regularUrl)) {
+            data.postData.content = postContent.replace(regularUrl, embed);
+        }
 
-		callback(null, data);
-	};
+        callback(null, data);
+    };
 
-	Twitter.init = {
+    Twitter.init = {
         global: {
             addNavigation: function(custom_header, callback) {
                         custom_header.navigation.push({
                         class: 'hidden',
                         route: "",
-                        text: '<script language="javascript" src="//platform.twitter.com/widgets.js"></script>'
+                        text: '<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>'
                     });
 
                 callback(null, custom_header);
@@ -29,5 +29,5 @@
         }
     },
 
-	module.exports = Twitter;
+    module.exports = Twitter;
 }(module));
