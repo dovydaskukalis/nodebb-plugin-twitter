b378af07587387a7d6cedd9875160b27b8f8c6e6
Alessio Cimarelli <alessio.cimarelli@gmail.com>
2015-09-21 13:49:13 +0000

    Update regex to take into account rel attributes if present, fix issue https://github.com/dovydaskukalis/nodebb-plugin-twitter/issues/6

--- library.js
+++ library.js
@@ -5,7 +5,7 @@
         embed = '<blockquote class="content twitter-tweet" lang="en"><a href="https://twitter.com/$1/status/$2"></a></blockquote><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>';
 
     Twitter.parse = function(data, callback) {
-        var regularUrl = /<a href="(?:https?:\/\/)?(?:twitter\.com)\/([^\/"\s]*)\/statuse?s?\/([^\/"\s]*)(\/photo\/1|)">.+?<\/a>/g
+        var regularUrl = /<a href="(?:https?:\/\/)?(?:twitter\.com)\/([^\/"\s]*)\/statuse?s?\/([^\/"\s]*)(\/photo\/\d|)".*?>.+?<\/a>/g
         var postContent = data && data.postData && data.postData.content;
 
         if (postContent && postContent.match(regularUrl)) {
