3d4dba621c68e85243822b47011e7da0e57e4005
dovydaskukalis <dovydas.kukalis@gmail.com>
2015-10-23 20:45:06 +0100

    compatibility with latest nodebb version

--- README.md
+++ README.md
@@ -4,15 +4,15 @@ This NodeBB plugin allows users to embed tweets inline in their posts.
 
 ## Installation
 
-    npm install nodebb-plugin-twitter
+	npm install nodebb-plugin-twitter
 
 ## Using
 
 All tweet URLs for example https://twitter.com/NodeBB/status/422901451896025089 will be automatically parsed as embedded tweet.
 
 You can get tweet URL by clicking on tweet's time and copying page URL in your browser.
-![](http://i.imgur.com/ZkF0Mig.png) 
+![](http://i.imgur.com/ZkF0Mig.png)
 
 ## Screenshots
 
-![twitter-plugin.png](http://i.imgur.com/8raiDZj.png) 
+![twitter-plugin.png](http://i.imgur.com/sTBEVrK.png) 
--- library.js
+++ library.js
@@ -1,33 +1,18 @@
 (function(module) {
-    "use strict";
+	"use strict";
 
-    var Twitter = {},
-        embed = '<blockquote class="content twitter-tweet" lang="en"><a href="https://twitter.com/$1/status/$2"></a></blockquote><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>';
+	var Twitter = {},
+		embed = '<blockquote class="content twitter-tweet" lang="en"><a href="https://twitter.com/$1/status/$2"></a></blockquote><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>';
 
-    Twitter.parse = function(data, callback) {
-        var regularUrl = /<a href="(?:https?:\/\/)?(?:twitter\.com)\/([^\/"\s]*)\/statuse?s?\/([^\/"\s]*)(\/photo\/1|)">.+?<\/a>/g
-        var postContent = data && data.postData && data.postData.content;
+	Twitter.parse = function(data, callback) {
+		var regularUrl = /<a href="(?:https?:\/\/)?(?:twitter\.com)\/([^\/"\s]*)\/statuse?s?\/([^\/"\s]*)(\/photo\/\d|)".*?>.+?<\/a>/g
+		var postContent = data && data.postData && data.postData.content;
 
-        if (postContent && postContent.match(regularUrl)) {
-            data.postData.content = postContent.replace(regularUrl, embed);
-        }
+		if (postContent && postContent.match(regularUrl)) {
+			data.postData.content = postContent.replace(regularUrl, embed);
+		}
+		callback(null, data);
+	};
 
-        callback(null, data);
-    };
-
-    Twitter.init = {
-        global: {
-            addNavigation: function(custom_header, callback) {
-                        custom_header.navigation.push({
-                        class: 'hidden',
-                        route: "",
-                        text: '<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>'
-                    });
-
-                callback(null, custom_header);
-            }
-        }
-    },
-
-    module.exports = Twitter;
+	module.exports = Twitter;
 }(module));
